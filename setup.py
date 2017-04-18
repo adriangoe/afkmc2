d81832295dfec3d87b352f3705100204cc24bb5a
Adrian Goedeckemeyer <adrian@minerva.kgi.edu>
2017-04-18 15:24:10 -0300

    Add requirement

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'afkmc2',
-  packages = ['afkmc2'],
-  version = '0.1',
-  description = 'Assumption Free and Efficient K-Means Seeding',
-  author = 'Adrian Goedeckemeyer',
-  author_email = 'adrian@minerva.kgi.edu',
-  url = 'https://github.com/adriangoe/afkmc2',
-  download_url = 'https://github.com/adriangoe/afkmc2/archive/0.1.tar.gz',
-  keywords = ['kmeans', 'seeding', 'sklearn', 'numpy'],
-  classifiers = [],
-  install_requires=[
-      "numpy",
-  ],
+    name = 'afkmc2',
+    packages = ['afkmc2'],
+    version = '0.1',
+    description = 'Assumption Free and Efficient K-Means Seeding',
+    author = 'Adrian Goedeckemeyer',
+    author_email = 'adrian@minerva.kgi.edu',
+    url = 'https://github.com/adriangoe/afkmc2',
+    download_url = 'https://github.com/adriangoe/afkmc2/archive/0.1.tar.gz',
+    keywords = ['kmeans', 'seeding', 'sklearn', 'numpy'],
+    classifiers = [],
+    install_requires=[
+        "numpy",
+    ],
 )
